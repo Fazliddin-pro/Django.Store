57b754d967b37798980b2fb9459521acd59319b9
Fazliddin <sarofiddinovfazliddin72@gmail.com>
2024-12-08 19:53:00 +0500

    After adding fixtures for goods

--- goods/views.py
+++ goods/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-
+from django.db.models import Count
+from .models import Categories
 
 def catalog(request):
+    # Получаем все категории с количеством товаров, связанных с каждой категорией
+    categories = Categories.objects.annotate(total_products=Count('products')).order_by('id') # 'products' - это обратная связь
+
     context = {
         'title': 'Aranoz - Catalog',
+        'categories': categories,
     }
     return render(request, 'goods/category.html', context)
+
 
 
 def product(request):
