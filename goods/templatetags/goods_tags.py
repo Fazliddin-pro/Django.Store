b422c5dfa4bebdc3939b15a4e98b953c3fd0e7de
Fazliddin <sarofiddinovfazliddin72@gmail.com>
2024-12-11 14:50:07 +0500

    Simple search and pagination created

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.db.models import Count
+from django.utils.http import urlencode
 
 from goods.models import Categories
 
@@ -10,3 +11,9 @@ register = template.Library()
 def tag_categories():
     return Categories.objects.annotate(total_products=Count("products")).order_by("id") #'products' - это обратная связь
 
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -1,21 +1,53 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 
-from .models import Categories, Products
+from goods.utils import q_search
+
+from .models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug=None):
 
-    goods = Products.objects.all()
+    page = request.GET.get('page', 1)
+    order_by = request.GET.get('order_by', 'default')
+    on_sale = request.GET.get('on_sale', None)
+    query = request.GET.get('q', None)
+
+    # Получаем товары в зависимости от категории
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+
+    # Фильтрация по скидке
+    if on_sale:  # Проверяем, если чекбокс был установлен
+        goods = goods.filter(discount__gt=0)
+
+    # Сортировка по цене
+    if order_by != 'default':
+        goods = goods.order_by(order_by)
+
+    # Пагинация
+    paginator = Paginator(goods, 6)
+    current_page = paginator.get_page(page)
 
     context = {
         "title": "Aranoz - Catalog",
-        "goods": goods,
+        "goods": current_page,
+        "category_slug": category_slug,
     }
+
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
     context = {
         "title": "Aranoz - Product",
+        "product": product
     }
     return render(request, "goods/product.html", context)
